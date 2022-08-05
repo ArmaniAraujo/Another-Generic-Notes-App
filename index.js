c72da932e863d1333077a68a472e216358160b07
Armani Araujo <armanidarren@gmail.com>
2022-08-05 14:50:36 -0400

    Updated code to add users to db

--- index.js
+++ index.js
@@ -4,21 +4,19 @@ const path = require('path')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const User = require('./models/User')
+const Joi = require('@hapi/joi')
 
 // Configures/grabs passwords/links/DB connection from .env
 dotenv.config()
-
-const user = {
-    name: "Armani Araujo",
-    phone: 6476476476,
-    age: 98
-}
 
 // Connect to DB
 
 mongoose.connect(process.env.DB_CONNECT,
     { useNewUrlParser: true },
-    () => console.log("connected to db!"));
+    (err) => {
+        if(err) console.log(err)
+        else console.log("connected to db!")
+    });
 
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, './public')));
@@ -48,7 +46,27 @@ app.listen(3000, () => {
     console.log('http://localhost:3000')
 })
 
-//app.use(express.json())
+
+/*
+    Validation schema to validate user 
+    information before saving to db
+*/
+const userSchema = Joi.object({
+    name: Joi.string()
+        .min(6)
+        .required(),
+    email: Joi.string()
+        .min(6)
+        .email()
+        .required(),
+    password: Joi.string()
+        .min(6)
+        .required()
+});
+
+
+
+app.use(express.json())
 
 // app.use(express.json({  extended: true }));
 
@@ -56,30 +74,46 @@ app.listen(3000, () => {
 app.use(express.urlencoded({  extended: true }));
 
 app.post('/register', async (req, res) => {
+
+    
+    
+    const validation = userSchema.validate(req.body);
+    res.send(validation)
+    const { error } = userSchema.validate(req.body);
+    if (error)
+        return res.status(400).send(error.details[0].message);
+
+// creating a Mongoose User object
     const user = new User ({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
     })
 
-    console.log(req.body.name);
-    console.log(req.body.email);
-    console.log(req.body.password);
+    console.log("Name:", user.name);
+    console.log("Email:", user.email);
+    console.log("Password:", user.password);
 
-/*
     try {
         const savedUser = await user.save()
-        res.send(savedUser)
-    } catch(err) { res.status(400).semd(err) }
-    */
+        res.send(console.log("savedUser"))
+    } catch (err) {console.log(err.message)}
+        //    } catch(err) { res.status(400).send(err) }
 
-    res.send(req.body);
+
 
 })
+
+
+
 
 app.post('/userlogin', async (req, res) => {
     console.log("username: ", req.body.username);
     console.log("password: ", req.body.password);
     res.render('home', { 'username': req.body.username });
+
+    const emailExists = await User.findOne({ email: req.body.email })
+    if (emailExists) console.log("Email exists") 
+    else console.log("DNE")
 })
 
